b54367f1cd8ed563f5e4293376eb639340c9a0b3
ludmila.machado <myllamachaado@gmail.com>
2022-08-08 22:25:51 -0300

    Initial commit

---
+++ src/java/dominio/Mentoria.java
@@ -0,0 +1,32 @@
+package java;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo{
+
+    LocalDate data;
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+}
+
